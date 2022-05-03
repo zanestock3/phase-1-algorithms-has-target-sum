d7bfd4c5c01729bc124de27038a1007b4a7f6cb7
Zane Stock <zanestock3@gmail.com>
2022-05-03 07:36:36 -0500

    commit tree 9e2693a5af72ebc680afa1e8e4a3c0078fc5afd7

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const secondary = (target - array[i])
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === secondary) {
+        return true
+      }
+    }
+  }
+  return false
 }
-
 /* 
   Write the Big O time complexity of your function here
 */
